24c3c58425e4d2c2083ce3a04429ab968b36b55d
devilseye <666devilseye666@gmail.com>
2020-01-03 03:59:07 +0400

    Initial commit

---
+++ src/test/java/devilseye/cocktailadvisor/backend/CocktailAdvisorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package devilseye.cocktailadvisor.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailAdvisorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
